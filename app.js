a5a8eacbf5c22e8374e1d90aa9f90cf9d7505a65
Ana Keri <ana.keri@fer.hr>
2023-12-18 04:04:12 +0100

    Adding index.js and changes to package.json

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then(registration => {
       console.log('Service Worker registered with scope:', registration.scope);
